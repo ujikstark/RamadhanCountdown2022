70f7046020ffc66c0d48a30aa28b48f63941c1cb
ujikstark <ujikbauk@gmail.com>
2021-09-04 17:21:46 +0700

    adding bar style

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const minutesElement = document.querySelector('.minutes h3');
 const hoursElement = document.querySelector('.hours h3');
 const daysElement = document.querySelector('.days h3');
 
+const secondsBar = document.getElementById('seconds');
+const minutesBar = document.getElementById('minutes');
+const hoursBar = document.getElementById('hours');
+const daysBar = document.getElementById('days');
 
 var destinationDate = new Date("April 2, 2022 08:00:00").getTime();
 
@@ -25,5 +29,15 @@ var x = setInterval(function() {
     minutesElement.innerHTML = minutes;
     hoursElement.innerHTML = hours;
     daysElement.innerHTML = days;
+    
+    // calculate the bar
+    secondsBar.style.width = (seconds*100/60)+'%';
+    minutesBar.style.width = (minutes*100/60)+'%';
+    hoursBar.style.width = (hours*100/24)+'%';
+    daysBar.style.width = Math.ceil(days*100/356)+'%';
     // console.log(d.getSeconds());
+    
+    // display the bar
+    
 }, 1000);
+
